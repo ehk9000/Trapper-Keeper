395dd52998f0ea80d3341633824f5a025e47b188
Erik Konner <erikkonner@gmail.com>
2019-05-20 15:45:38 -0600

    Created boiler plate file structure, imported dependancies in index.js
    Co-authored-by: Kristen Hallstrom <Klhalls89@gmail.com>
    Co-authored-by: tkbacher <tiffanybacher@gmail.com>

---
+++ src/containers/App/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>
+          Trapper Keeper!
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import { BrowserRouter } from 'react-router-dom';
+// import {rootReducer} from './reducers';
+import './styles/index.scss';
+import App from './containers/App/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const router = (
+  // <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  // </Provider>
+)
+
+ReactDOM.render(router, document.getElementById('root'));
