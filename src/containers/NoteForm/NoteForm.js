3ffc3c68e9150752b827b2da380792e4475978ad
Kristen Hallstrom <klhalls89@gmail.com>
2019-05-21 21:06:44 -0600

    Add button to note form and an onclick function

--- src/containers/NoteForm/NoteForm.js
+++ src/containers/NoteForm/NoteForm.js
@@ -34,12 +34,20 @@ class NoteForm extends Component {
     }
   }
 
+  onClick = () => {
+    this.setState({
+      title: '',
+      listItem: ''
+    })
+  }
+
   render() {
     const itemInput = 
       <input 
         type="text"
         placeholder="List item"
         name="listItem"
+        value={this.state.listItem}
         onChange={this.handleChange}
         onKeyPress={this.handleKeyPress}
         onBlur={this.updateList} />
@@ -53,6 +61,7 @@ class NoteForm extends Component {
           value={this.state.title}
           onChange={this.handleChange} />
         {itemInput}
+        <button onClick={this.onClick}>add</button>
       </section>
     );
   }
