3d77dcb41d9724a59cac47a1401fe1b8f6e31dc3
tiffanybacher <tiffanybacher@gmail.com>
2019-05-23 06:42:12 -0600

    Create thunk to post note and add to redux store

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,19 +1,19 @@
 export const addNote = (note) => ({
   type: 'ADD_NOTE',
   note
-})
+});
 
 export const setNoteTitle = (title) => ({
   type: 'SET_NOTE_TITLE',
   title
-})
+});
 
 export const setListItem = (item) => ({
   type: 'SET_LIST_ITEM',
   item
-})
+});
 
 export const toggleComplete = (bool) => ({
   type: 'TOGGLE_COMPLETE',
   complete: bool
-})
+});
