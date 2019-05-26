ea54181fa7862395a09751198182f0e5e8a88ae6
Kristen Hallstrom <klhalls89@gmail.com>
2019-05-26 10:47:19 -0600

    Beging test update note

--- src/reducers/notesReducer.test.js
+++ src/reducers/notesReducer.test.js
@@ -3,7 +3,8 @@
 
  describe ('notesReducer', () => {
 
-    let note = { title: 'newNote', listItem: 'get peas' }
+    let note = { title: 'note', listItem: 'get cream', id: 1 };
+    let notes =[note, { title: 'newNote', listItem: 'get salt', id: 2 }]
 
     it('should return default state', () => {
       const expected = [];
@@ -12,11 +13,49 @@
       expect(result).toEqual(expected);
     })
 
-    it('should add note to state tree ',() => {
-      const expected = [note];
-      const action = actions.addNote(note);
-      const result = notesReducer([], action)
+    describe('ADD_NOTE', () => {
 
-      expect(result).toEqual(expected)
+      it('should add note to state tree ', () => {
+        const expected = [note];
+        const action = actions.addNote(note);
+        const result = notesReducer([], action);
+
+        expect(result).toEqual(expected);
+      });
     });
- })
+
+    describe('ADD_ALL_NOTES', () => {
+
+      it('should add all notes to state tree', () => {
+        const expected = notes;
+        const action = actions.addAllNotes(notes);
+        const result = notesReducer([], action);
+        
+        expect(result).toEqual(expected);
+      }); 
+    });
+
+    describe('UPDATE_NOTE', () => {
+
+      it('should return the original matching note not found', => {
+        let updateNote = { title: 'note', listItem: 'get almonds', id: 4 }
+        action = actions.updateNote(updateNote)
+        const expected = note
+      });
+
+      it.skip('should update note on state tree', () => {
+        let updateNote = { title: 'note', listItem: 'get almonds', id: 1 };
+        const action = actions.updateNote(updateNote);
+        const updateNotes = [updateNote, { title: 'newNote', listItem: 'get salt', id: 2 }];
+        const expected = updateNotes;
+        const result = notesReducer([], action); 
+
+        expect(result).toEqual(expected)
+
+      });
+
+
+
+    });
+
+ });
