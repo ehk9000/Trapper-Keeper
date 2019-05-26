bb17040e2dd87444a1322e1db30e2cd6daa09345
Erik Konner <erikkonner@gmail.com>
2019-05-26 15:18:08 -0600

    Passing tests for actions: addAllNotes, addNote, updateNote, setLoading, & setError

--- src/actions/actions.test.js
+++ src/actions/actions.test.js
@@ -1,24 +1,92 @@
 import * as actions from './index'
 
 describe('actions', () => {
-  it('should return a type of GET_NOTES with a notes array', () => {
-    const note = [
+  it('should return a type of ADD_ALL_NOTES with a notes array', () => {
+    const notes = [
       {
       title: 'Groceries',
-      listItem: 'blah blah'}, 
+      list: [{
+        item: 'milk', completed: false, id:3493}], 
+      id:1234 },
     {
       title: 'Todo',
-      listItem: 'blah'
+      list: [{
+        item: 'eggs', completed: false, id: 12355
+      }],
+      id:234239
     }
-  ]
+  ];
+
   const expected  = {
-    type: 'ADD_NOTE',
-    note
+    type: 'ADD_ALL_NOTES',
+    notes
   }
 
-  const result = actions.addNote(note)
+  const result = actions.addAllNotes(notes)
 
   expect(result).toEqual(expected)
+  });
+  
+  it('should return a type of setLoading with a boolean', () => {
+    const bool = true;
+    const expected = {
+      type: 'SET_LOADING',
+      isLoading: bool
+    }
 
-  })
-})
+    const result = actions.setLoading(bool);
+
+    expect(result).toEqual(expected);
+  });
+
+  it('should return a type of addNote with a new note', () => {
+    const note = {
+      title: 'Chores',
+        list: [{
+          item: 'laundry', completed: false, id: 32343
+        }],
+          id: 1444
+    }
+
+    const expected = {
+      type:'ADD_NOTE',
+      note
+    }
+
+    const result = actions.addNote(note);
+
+    expect(result).toEqual(expected);
+  });
+
+  it('should return a type of updateNote with a new note', () => {
+    const note = {
+      title: 'Chores',
+      list: [{
+        item: 'laundry', completed: false, id: 32343
+      }],
+      id: 1444
+    };
+
+    const expected = {
+      type: 'UPDATE_NOTE',
+      note
+    };
+
+    const result = actions.updateNote(note);
+
+    expect(result).toEqual(expected);
+  });
+
+  it('should return a type of setError with an error message', () => {
+    const error = 'Something went wrong';
+    const expected = {
+      type: 'SET_ERROR',
+      error
+    };
+
+    const result = actions.setError(error);
+
+    expect(result).toEqual(expected);
+  });
+  
+});
