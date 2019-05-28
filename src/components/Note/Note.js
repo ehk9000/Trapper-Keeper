c6b51996e1fce7fd9daedef1da06f61d0b17e3b9
Erik Konner <erikkonner@gmail.com>
2019-05-28 14:17:51 -0600

    Completed Items display in bottom of note in note container

--- src/components/Note/Note.js
+++ src/components/Note/Note.js
@@ -3,8 +3,15 @@ import {Link} from 'react-router-dom'
 
 const Note = (props) => {
   let { title, list, id} = props;
-  let listItems = list.map(item => {
-    return <p key={item.id} className={item.completed ? 'completed-view' : 'note-item'}>{item.item}</p>
+
+  let incompleteItems = list.filter(item => !item.completed);
+  incompleteItems = incompleteItems.map(item => {
+    return <p key={item.id} className="note-item">{item.item}</p>
+  });
+
+  let completedItems = list.filter(item => item.completed);
+  completedItems = completedItems.map(item => {
+    return <p key={item.id} className="completed-view">{item.item}</p>
   });
 
   return (
@@ -12,7 +19,8 @@ const Note = (props) => {
       <Link to={`/notes/${id}`} className="noteLink"> 
         <article>
           <h3>{title}</h3>
-          {listItems}
+          {incompleteItems}
+          {completedItems}
         </article>
       </Link>
       <i className="far fa-trash-alt delete-btn" onClick={() => props.fetchDeleteNote(id)} ></i>
